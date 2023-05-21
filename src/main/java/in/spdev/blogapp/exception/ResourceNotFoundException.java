4944f2a6c776f197d43beef7cca0a5f40b057ddc
codebuffdev <codebuffdev@gmail.com>
2023-05-21 14:45:46 +0530

    CustomException

---
+++ src/main/java/in/spdev/blogapp/exception/ResourceNotFoundException.java
@@ -0,0 +1,22 @@
+package in.spdev.blogapp.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResourceNotFoundException extends RuntimeException {
+
+    private String resourceName;
+    private String fieldName;
+    private Integer fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Integer fieldValue) {
+        super(String.format("%s not found with %s : %d",resourceName,fieldName,fieldValue));
+        this.resourceName=resourceName;
+        this.fieldName=fieldName;
+        this.fieldValue=fieldValue;
+    }
+
+
+}
