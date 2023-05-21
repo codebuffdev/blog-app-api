595db73a4b9161a365cb3247bf494b62f950c8c5
codebuffdev <codebuffdev@gmail.com>
2023-05-21 17:59:56 +0530

    ApiResponse

---
+++ src/main/java/in/spdev/blogapp/dto/ApiResponse.java
@@ -0,0 +1,15 @@
+package in.spdev.blogapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private String response;
+    private boolean status;
+}
