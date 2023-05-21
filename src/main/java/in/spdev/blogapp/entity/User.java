d00a856c1cd6bd2e53e4a2728353662dc55c28a7
codebuffdev <codebuffdev@gmail.com>
2023-05-21 14:45:32 +0530

    UserEntity

---
+++ src/main/java/in/spdev/blogapp/entity/User.java
@@ -0,0 +1,27 @@
+package in.spdev.blogapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user_name")
+    private String name;
+    private String password;
+    private String email;
+    private String about;
+}
